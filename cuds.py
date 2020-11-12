8d1dfb89be164d4d78be3a335ae8ff5de147b281
Carl Simon Adorf <carl.simon.adorf@gmail.com>
2020-11-12 09:06:51 +0000

    Enable CUDS import from OSP callback.

--- cuds.py
+++ cuds.py
@@ -1,6 +1,8 @@
 import json
+from urllib.parse import urlsplit, parse_qs
 
 import ase
+import requests
 import traitlets
 import ipywidgets as ipw
 
@@ -15,7 +17,9 @@ class CUDSImporter(ipw.HBox):
     def __init__(self, **kwargs):
         self.file_upload = ipw.FileUpload(multiple=False, layout={'width': 'initial'})
         self.file_upload.observe(self._on_file_upload, names='value')
-        super().__init__(children=[self.file_upload], **kwargs)
+        self.callback_indicator = ipw.Valid(description='osp-callback')
+
+        super().__init__(children=[self.file_upload, self.callback_indicator], **kwargs)
         
     @staticmethod
     def _to_ase(cuds_object):
@@ -33,3 +37,17 @@ class CUDSImporter(ipw.HBox):
             assert item['metadata']['type'] == 'application/json'
             cuds_object = deserialize(json.loads(item['content'].decode('utf-8')))
             self.structure = self._to_ase(cuds_object)
+
+    def from_callback(self, url):
+        with self.hold_trait_notifications():
+            try:
+                query = parse_qs(urlsplit(url).query)
+                osp_callback = query['osp_callback'][0]
+            except (KeyError, IndexError):
+                self.callback_indicator.value = False
+            else:
+                response = requests.get(osp_callback)
+                response.raise_for_status()
+                cuds_object = deserialize(response.json())
+                self.structure = self._to_ase(cuds_object)
+                self.callback_indicator.value = True
