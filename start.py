41a100dcca77f24a763fec31ccee2c3dc7cdd739
Carl Simon Adorf <carl.simon.adorf@gmail.com>
2021-02-01 13:59:43 +0000

    Refactor start module.

--- start.py
+++ start.py
@@ -2,17 +2,11 @@ import ipywidgets as ipw
 
 
 def get_start_widget(appbase, jupbase, notebase):
-    # http://fontawesome.io/icons/
-    template = """
+    return ipw.HTML(
+        f"""
     <div align="center">
         <a href="{appbase}/qe.ipynb" target="_blank">
             <img src="https://gitlab.com/QEF/q-e/raw/develop/logo.jpg" height="120px" width=243px">
         </a>
-    </div>
-    """
-
-    html = template.format(appbase=appbase, jupbase=jupbase, notebase=notebase)
-    return ipw.HTML(html)
-
-
-# EOF
+    </div>"""
+    )
