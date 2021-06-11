d14ba51ad4113ba48ffb5917db0dc1f073101279
Carl Simon Adorf <carl.simon.adorf@gmail.com>
2021-06-11 14:24:06 +0000

    Remove obsolete SSSPInstallWidget.

--- aiidalab_qe/pseudos.py
+++ aiidalab_qe/pseudos.py
@@ -1,112 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
-from subprocess import run
-from threading import Thread
 
 import ipywidgets as ipw
 import traitlets
-from aiida import orm, plugins
 
 from aiidalab_qe.parameters import DEFAULT_PARAMETERS
-
-SsspFamily = plugins.GroupFactory("pseudo.family.sssp")
-
-
-class Spinner(ipw.HTML):
-    """Widget that shows a simple spinner if enabled."""
-
-    enabled = traitlets.Bool()
-
-    def __init__(self, spinner_style=None):
-        self.spinner_style = f' style="{spinner_style}"' if spinner_style else ""
-        super().__init__()
-
-    @traitlets.default("enabled")
-    def _default_enabled(self):  # pylint: disable=no-self-use
-        return False
-
-    @traitlets.observe("enabled")
-    def _observe_enabled(self, change):
-        """Show spinner if enabled, otherwise nothing."""
-        if change["new"]:
-            self.value = (
-                f"""<i class="fa fa-spinner fa-pulse"{self.spinner_style}></i>"""
-            )
-        else:
-            self.value = ""
-
-
-class SSSPInstallWidget(ipw.HBox):
-
-    installed = traitlets.Bool().tag(readonly=True)
-    busy = traitlets.Bool().tag(readonly=True)
-
-    base_url = "http://legacy-archive.materialscloud.org/file/2018.0001/v3"
-
-    def __init__(self, **kwargs):
-
-        self.install_button = ipw.Button(
-            description="Install pseudos",
-            button_style="warning",
-            icon="cloud-download",
-            tooltip="Download and install the SSSP pseudo potential families.",
-            disabled=True,
-            layout=ipw.Layout(width="140px"),
-        )
-        self.install_button.on_click(lambda _: self.download())
-
-        self.spinner = Spinner()
-        ipw.dlink((self, "busy"), (self.spinner, "enabled"))
-
-        kwargs.setdefault("layout", ipw.Layout(width="180px"))
-
-        super().__init__(children=[self.install_button, self.spinner], **kwargs)
-        self._refresh_installed()
-
-    def _refresh_installed(self):
-        try:
-            self.set_trait("busy", True)
-            all_pseudo_families = (
-                orm.QueryBuilder().append(SsspFamily, project="label").all(flat=True)
-            )
-            self.installed = (
-                "SSSP/1.1/PBE/precision" in all_pseudo_families
-                and "SSSP/1.1/PBE/efficiency" in all_pseudo_families
-                and "SSSP/1.1/PBEsol/precision" in all_pseudo_families
-                and "SSSP/1.1/PBEsol/efficiency" in all_pseudo_families
-            )
-        finally:
-            self.set_trait("busy", False)
-
-    @traitlets.default("busy")
-    def _default_busy(self):
-        return False
-
-    @traitlets.observe("busy")
-    def _observe_busy(self, change):
-        self.install_button.disabled = change["new"]
-
-    @traitlets.observe("installed")
-    def _observe_installed(self, change):
-        self.install_button.layout.visibility = "hidden" if change["new"] else "visible"
-
-    def _download(self):
-        try:
-            self.set_trait("busy", True)
-
-            functionals = ["PBE", "PBEsol"]
-            protocols = ["efficiency", "precision"]
-
-            for func in functionals:
-                for prot in protocols:
-                    run(["aiida-pseudo", "install", "sssp", "-x", func, "-p", prot])
-            self._refresh_installed()
-        finally:
-            self.set_trait("busy", False)
-
-    def download(self):
-        thread = Thread(target=self._download)
-        thread.start()
 
 
 class PseudoFamilySelector(ipw.VBox):
@@ -150,7 +48,6 @@ class PseudoFamilySelector(ipw.VBox):
         well-established generalised gradient approximation (GGA) functionals:
         PBE and PBEsol.</div>"""
     )
-    installed = traitlets.Bool()
     disabled = traitlets.Bool()
 
     value = traitlets.Unicode(
@@ -190,10 +87,6 @@ class PseudoFamilySelector(ipw.VBox):
             layout=ipw.Layout(max_width="80%"),
         )
         self.protocol_selection.observe(self.set_value_trait, "value")
-
-        # Setup pseudofamily potential selection group:
-        self.sssp_install_widget = SSSPInstallWidget()
-        ipw.dlink((self.sssp_install_widget, "installed"), (self, "installed"))
 
         self.dft_functional_box = ipw.VBox(
             children=[
