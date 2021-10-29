a676098b0276eac09ff90f33c173a58e984de6be
Simon Adorf <carl.simon.adorf@gmail.com>
2021-10-29 09:30:26 +0000

    Make download archive function resistent against race conditions.
    And someother minor refactoring.

--- aiidalab_qe/node_view.py
+++ aiidalab_qe/node_view.py
@@ -21,6 +21,7 @@ from aiida.orm import CalcJobNode, Node, WorkChainNode
 from aiidalab_widgets_base import ProcessMonitor, register_viewer_widget
 from aiidalab_widgets_base.viewers import StructureDataViewer
 from ase import Atoms
+from filelock import FileLock, Timeout
 from IPython.display import HTML, display
 from jinja2 import Environment
 from monty.json import MontyEncoder, jsanitize
@@ -29,8 +30,6 @@ from widget_bandsplot import BandsPlotWidget
 
 from aiidalab_qe import static
 from aiidalab_qe.report import generate_report_dict
-
-EXPORT_DIR = "export"
 
 
 class MinimalStructureViewer(ipw.VBox):
@@ -207,7 +206,8 @@ class SummaryView(ipw.VBox):
 
 
 class WorkChainOutputs(ipw.VBox):
-    def __init__(self, node, **kwargs):
+    def __init__(self, node, export_dir=None, **kwargs):
+        self.export_dir = Path.cwd().joinpath("exports")
 
         if node.process_label != "QeAppWorkChain":
             raise KeyError(str(node.node_type))
@@ -254,33 +254,46 @@ class WorkChainOutputs(ipw.VBox):
 
     def _download_archive(self, _):
 
-        for file in Path(EXPORT_DIR).iterdir():
-            file.unlink()
+        fn_archive = self.export_dir.joinpath(str(self.node.uuid)).with_suffix(".zip")
+        fn_lockfile = fn_archive.with_suffix(".lock")
 
-        with TemporaryDirectory() as tmpdir:
-            tmp_dirpath = Path(tmpdir)
-            self._prepare_calcjob_io(self.node, tmp_dirpath)
-            shutil.make_archive(f"{EXPORT_DIR}/{self.node.uuid}", "zip", tmp_dirpath)
+        try:
+            # Create exports archive directory.
+            fn_archive.parent.mkdir(parents=True, exist_ok=True)
+            # Try to obtain lock for creating archive...
+            with FileLock(fn_lockfile, timeout=0):
+                # Check whether archive file already exists.
+                if not fn_archive.is_file():
+                    # Create archive file.
+                    with TemporaryDirectory() as tmpdir:
+                        self._prepare_calcjob_io(self.node, Path(tmpdir))
+                        shutil.make_archive(fn_archive.with_suffix(""), "zip", tmpdir)
+            Path(fn_lockfile).unlink()  # Delete lock file.
+        except Timeout:
+            # Failed to obtain lock, presuming some other process is working on it.
+            with FileLock(fn_lockfile, timeout=20):
+                assert fn_archive.is_file()
 
         id = f"dl_{self.node.uuid}"
 
         display(
             HTML(
                 f"""
-            <html>
-            <body>
-            <a id="{id}" download="calcjob_io.zip"
-            href="{EXPORT_DIR}/{self.node.uuid}.zip"></a>
+        <html>
+        <body>
+        <a
+            id="{id}"
+            href="{fn_archive.relative_to(Path.cwd())}"
+            download="{fn_archive.stem}"
+        ></a>
+        <script>
+        (function download() {{document.getElementById("{id}").click();
+        }})()
+        </script>
 
-            <script>
-            (function download() {{
-            document.getElementById('{id}').click();
-            }})()
-            </script>
-
-            </body>
-            </html>
-            """
+        </body>
+        </html>
+        """
             )
         )
 
