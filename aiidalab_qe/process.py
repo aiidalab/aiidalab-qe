b2bc537fb08961a428d7a5dd40583c8c07dcb371
Jason.Yu <jusong.yeu@gmail.com>
2022-01-24 18:20:21 +0100

    Show more details in the workchain selector (#176)
    * Add more detail to the workchain info as part of the workchain selector
    
    Fixes #160
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- aiidalab_qe/process.py
+++ aiidalab_qe/process.py
@@ -25,7 +25,7 @@ class WorkChainSelector(ipw.HBox):
     # use `None` as setting the widget's value to None will lead to "no selection".
     _NO_PROCESS = object()
 
-    FMT_WORKCHAIN = "{wc.pk:6}{wc.ctime:>10}\t{wc.state:<16}\t{wc.formula}"
+    FMT_WORKCHAIN = "{wc.pk:6}{wc.ctime:>10}\t{wc.state:<16}\t{wc.formula} \t {wc.relax_info} \t {wc.properties_info}"
 
     def __init__(self, **kwargs):
         self.work_chains_prompt = ipw.HTML("<b>Select workflow or start new:</b>&nbsp;")
@@ -62,6 +62,8 @@ class WorkChainSelector(ipw.HBox):
         ctime: str
         state: str
         formula: str
+        relax_info: str
+        properties_info: str
 
     @classmethod
     def find_work_chains(cls):
@@ -80,7 +82,27 @@ class WorkChainSelector(ipw.HBox):
         for process in projected[1:]:
             pk = process[0]
             formula = load_node(pk).inputs.structure.get_formula()
-            yield cls.WorkChainData(formula=formula, *process)
+            if "relax" in load_node(pk).inputs:
+                relax_info = "structure is relaxed"
+            else:
+                relax_info = "structure is not relaxed"
+
+            properties = []
+            if "pdos" in load_node(pk).inputs:
+                properties.append("pdos")
+            if "bands" in load_node(pk).inputs:
+                properties.append("bands")
+
+            if not properties:
+                properties_info = ""
+            else:
+                properties_info = f"properties on {', '.join(properties)}"
+            yield cls.WorkChainData(
+                formula=formula,
+                relax_info=relax_info,
+                properties_info=properties_info,
+                *process,
+            )
 
     @traitlets.default("busy")
     def _default_busy(self):
