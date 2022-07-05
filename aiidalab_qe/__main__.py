f5557317d4af9c75c9df73433e44bb8772fb86c0
Simon Adorf <simon.adorf@epfl.ch>
2022-07-05 13:56:29 +0000

    Refactor installation routines and allow to run them from the CLI.

---
+++ aiidalab_qe/__main__.py
@@ -0,0 +1,36 @@
+import click
+
+from .setup_codes import codes_are_setup
+from .setup_codes import install as install_qe_codes
+from .sssp import install as setup_sssp
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("-f", "--force", is_flag=True)
+def install_qe(force):
+    try:
+        for msg in install_qe_codes(force=force):
+            click.echo(msg)
+        assert codes_are_setup()
+        click.secho("Codes are setup!", fg="green")
+    except Exception as error:
+        raise click.ClickException(f"Failed to set up QE failed: {error}")
+
+
+@cli.command()
+def install_sssp():
+    try:
+        for msg, _ in setup_sssp():
+            click.echo(msg)
+        click.secho("SSSP pseudo potentials are installed!", fg="green")
+    except Exception as error:
+        raise click.ClickException(f"Failed to set up pseudo potentials: {error}")
+
+
+if __name__ == "__main__":
+    cli()
