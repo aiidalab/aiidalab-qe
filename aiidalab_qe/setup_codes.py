4e6df774468bd50f119063df8a6381935731575e
Jusong Yu <jusong.yeu@gmail.com>
2022-11-20 12:28:32 +0000

    Code setup using API

--- aiidalab_qe/setup_codes.py
+++ aiidalab_qe/setup_codes.py
@@ -7,7 +7,7 @@ from threading import Thread
 import ipywidgets as ipw
 import traitlets
 from aiida.common.exceptions import NotExistent
-from aiida.orm import load_code
+from aiida.orm import InstalledCode, load_code, load_computer
 from filelock import FileLock, Timeout
 
 from aiidalab_qe.widgets import ProgressBar
@@ -67,29 +67,18 @@ def _setup_code(code_name, computer_name="localhost"):
     try:
         load_code(f"{code_name}-{QE_VERSION}@localhost")
     except NotExistent:
-        # TODO replace with API setup after aiida-core 2.1 support
-        run(
-            [
-                "verdi",
-                "code",
-                "setup",
-                "--non-interactive",
-                "--label",
-                f"{code_name}-{QE_VERSION}",
-                "--description",
-                f"{code_name}.x ({QE_VERSION}) setup by AiiDAlab.",
-                "--input-plugin",
-                f"quantumespresso.{code_name}",
-                "--computer",
-                computer_name,
-                "--prepend-text",
-                f'eval "$(conda shell.posix hook)"\nconda activate {CONDA_ENV_PREFIX}\nexport OMP_NUM_THREADS=1',
-                "--remote-abs-path",
-                CONDA_ENV_PREFIX.joinpath("bin", f"{code_name}.x"),
-            ],
-            check=True,
-            capture_output=True,
+        localhost = load_computer(computer_name)
+        code = InstalledCode(
+            computer=localhost,
+            filepath_executable=CONDA_ENV_PREFIX.joinpath(
+                "bin", f"{code_name}.x"
+            ).as_posix(),
+            default_calc_job_plugin=f"quantumespresso.{code_name}",
+            prepend_text=f'eval "$(conda shell.posix hook)"\nconda activate {CONDA_ENV_PREFIX}\nexport OMP_NUM_THREADS=1',
         )
+        code.label = f"{code_name}-{QE_VERSION}"
+        code.description = f"{code_name}.x ({QE_VERSION} setup by AiiDAlab)"
+        code.store()
     else:
         raise RuntimeError(f"Code {code_name} (v{QE_VERSION}) is already setup!")
 
