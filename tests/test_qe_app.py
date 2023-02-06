56f542b849c4b7f86ba9713bbe3406c0126e90e5
Jusong Yu <jusong.yeu@gmail.com>
2023-02-06 11:54:30 +0100

    Bump isort to fix the failing pre-commit

--- tests/test_qe_app.py
+++ tests/test_qe_app.py
@@ -16,12 +16,16 @@ def test_qe_app_take_screenshot(selenium_driver, final_screenshot):
     driver.set_window_size(1920, 1485)
     time.sleep(15)
 
-def test_qe_app_select_silicon_and_confirm(selenium_driver, screenshot_dir, final_screenshot):
+
+def test_qe_app_select_silicon_and_confirm(
+    selenium_driver, screenshot_dir, final_screenshot
+):
     driver = selenium_driver("qe.ipynb", wait_time=30.0)
     driver.set_window_size(1920, 1485)
     driver.find_element(
         By.XPATH, "//*[text()='From Examples']"
     ).click()  # click `From Examples` tab for input structure
+    time.sleep(10)
     driver.find_element(By.XPATH, "//option[@value='Diamond']").click()
     time.sleep(10)
     driver.get_screenshot_as_file(
