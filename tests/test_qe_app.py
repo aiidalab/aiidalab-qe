c52a41ed6e55c1ab2d104af970e60bbad6c6b3b4
Jusong Yu <jusong.yeu@gmail.com>
2023-02-07 17:48:17 +0100

    f-d

--- tests/test_qe_app.py
+++ tests/test_qe_app.py
@@ -26,16 +26,13 @@ def test_qe_app_select_silicon_and_confirm(
 ):
     driver = selenium_driver("qe.ipynb", wait_time=30.0)
     driver.set_window_size(1920, 1485)
-    time.sleep(15)
-
-    driver.find_element(
-        By.XPATH, "//*[text()='From Examples']"
-    ).click()  # click `From Examples` tab for input structure
 
     element = WebDriverWait(driver, 60).until(
-        EC.element_to_be_clickable((By.XPATH, "//option[@value='Diamond']"))
+        EC.presence_of_element_located((By.XPATH, "//*[text()='From Examples']"))
     )
     element.click()
+
+    driver.find_element(By.XPATH, "//option[@value='Diamond']").click()
 
     driver.get_screenshot_as_file(
         str(Path.joinpath(screenshot_dir, "qe-app-select-diamond-selected.png"))
