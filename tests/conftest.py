ab93cdec63c2252e699b2252e9bab1e8cd7d547d
Jusong Yu <jusong.yeu@gmail.com>
2023-04-06 19:20:54 +0200

    fix ut

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,29 @@ def fixture_localhost(aiida_localhost):
     localhost = aiida_localhost
     localhost.set_default_mpiprocs_per_machine(1)
     return localhost
+
+
+@pytest.fixture
+def pw_code(aiida_local_code_factory):
+    """Return a `Code` configured for the pw.x executable."""
+    return aiida_local_code_factory(
+        label="pw@localhost", executable="bash", entry_point="quantumespresso.pw"
+    )
+
+
+@pytest.fixture
+def dos_code(aiida_local_code_factory):
+    """Return a `Code` configured for the dos.x executable."""
+    return aiida_local_code_factory(
+        label="dos@localhost", executable="bash", entry_point="quantumespresso.dos"
+    )
+
+
+@pytest.fixture
+def projwfc_code(aiida_local_code_factory):
+    """Return a `Code` configured for the projwfc.x executable."""
+    return aiida_local_code_factory(
+        label="projwfc@localhost",
+        executable="bash",
+        entry_point="quantumespresso.projwfc",
+    )
--- tests/test_submit_qe_workchain.py
+++ tests/test_submit_qe_workchain.py
@@ -2,7 +2,9 @@ import pytest
 
 
 @pytest.mark.usefixtures("aiida_profile_clean")
-def test_get_input_parameters(data_regression):
+def test_get_input_parameters(data_regression, pw_code, dos_code, projwfc_code):
+    from aiida import orm
+
     from aiidalab_qe.pseudos import PseudoFamilySelector
     from aiidalab_qe.steps import (
         KpointSettings,
@@ -18,7 +20,16 @@ def test_get_input_parameters(data_regression):
     submit_step.kpoints_settings = KpointSettings()
     submit_step.smearing_settings = SmearingSettings()
 
+    submit_step.pw_code.value = pw_code.uuid
+    submit_step.dos_code.value = dos_code.uuid
+    submit_step.projwfc_code.value = projwfc_code.uuid
+
     parameters = submit_step.get_input_parameters()
 
     # Check and validate the parameters
+    # The dict store code uuids, but we want to check the labels.
+    parameters["pw_code"] = orm.load_code(parameters["pw_code"]).label
+    parameters["dos_code"] = orm.load_code(parameters["dos_code"]).label
+    parameters["projwfc_code"] = orm.load_code(parameters["projwfc_code"]).label
+
     data_regression.check(parameters)
