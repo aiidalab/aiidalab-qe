2029267bf68121756763c45a317ba55e25d45a25
Jusong Yu <jusong.yeu@gmail.com>
2023-05-09 13:07:46 +0200

    Move module imports from __init__.py of package to app modules (#393)

--- aiidalab_qe/__init__.py
+++ aiidalab_qe/__init__.py
@@ -1,17 +1,8 @@
-"""Package for the AiiDAlab QE app."""
-
-from aiidalab_qe.app.process import WorkChainSelector
-from aiidalab_qe.app.steps import (
-    SubmitQeAppWorkChainStep,
-    ViewQeAppWorkChainStatusAndResultsStep,
-)
-from aiidalab_qe.app.structures import StructureSelectionStep
+"""Package for the AiiDAlab QE app version.
+Plugin (workflow) version will be read from the plugin's __init__.py file.
+"""
 from aiidalab_qe.version import __version__
 
 __all__ = [
-    "StructureSelectionStep",
-    "SubmitQeAppWorkChainStep",
-    "ViewQeAppWorkChainStatusAndResultsStep",
-    "WorkChainSelector",
     "__version__",
 ]
