baa025cb29307dfdd0b9f89197eb0833ce461a7d
Jusong Yu <jusong.yeu@gmail.com>
2023-05-18 13:47:50 +0000

    seprate workchain selector to be extendable

--- aiidalab_qe/app/process.py
+++ aiidalab_qe/app/process.py
@@ -1,32 +1,31 @@
 """Widgets related to process management."""
-from dataclasses import dataclass
-from threading import Event, Lock, Thread
+from dataclasses import make_dataclass
 
 import ipywidgets as ipw
-import traitlets
-from aiida.orm import load_node
+import traitlets as tl
+from aiida import orm
 from aiida.tools.query.calculation import CalculationQueryBuilder
 
 
 class WorkChainSelector(ipw.HBox):
     # The PK of a 'aiida.workflows:quantumespresso.pw.bands' WorkChainNode.
-    value = traitlets.Int(allow_none=True)
-
-    # When this trait is set to a positive value, the work chains are automatically
-    # refreshed every `auto_refresh_interval` seconds.
-    auto_refresh_interval = traitlets.Int()  # seconds
+    value = tl.Int(allow_none=True)
 
     # Indicate whether the widget is currently updating the work chain options.
-    busy = traitlets.Bool(read_only=True)
+    busy = tl.Bool(read_only=True)
 
     # Note: We use this class as a singleton to reset the work chains selector
     # widget to its default stage (no work chain selected), because we cannot
     # use `None` as setting the widget's value to None will lead to "no selection".
     _NO_PROCESS = object()
 
-    FMT_WORKCHAIN = "{wc.pk:6}{wc.ctime:>10}\t{wc.state:<16}\t{wc.formula} \t {wc.relax_info} \t {wc.properties_info}"
+    FMT_WORKCHAIN = "{wc.pk:6}{wc.ctime:>10}\t{wc.state:<16}"
 
-    def __init__(self, **kwargs):
+    BASE_FIELDS = [("pk", int), ("ctime", str), ("state", str)]
+    EXTRA_FIELDS = None
+
+    def __init__(self, process_label, **kwargs):
+        self.process_label = process_label
         self.work_chains_prompt = ipw.HTML(
             "<b>Select computed workflow or start a new one:</b>&nbsp;"
         )
@@ -40,13 +39,12 @@ class WorkChainSelector(ipw.HBox):
             transform=lambda pk: None if pk is self._NO_PROCESS else pk,
         )
 
+        if self.EXTRA_FIELDS is not None:
+            fmt_extra = "".join([f"{{wc.{field[0]}}}" for field in self.EXTRA_FIELDS])
+            self.FMT_WORKCHAIN += "\t" + fmt_extra
+
         self.refresh_work_chains_button = ipw.Button(description="Refresh")
         self.refresh_work_chains_button.on_click(self.refresh_work_chains)
-
-        self._refresh_lock = Lock()
-        self._refresh_thread = None
-        self._stop_refresh_thread = Event()
-        self._update_auto_refresh_thread_state()
 
         super().__init__(
             children=[
@@ -57,121 +55,67 @@ class WorkChainSelector(ipw.HBox):
             **kwargs,
         )
 
-    @dataclass
-    class WorkChainData:
-        pk: int
-        ctime: str
-        state: str
-        formula: str
-        relax_info: str
-        properties_info: str
+        self.refresh_work_chains()
 
-    @classmethod
-    def find_work_chains(cls):
+    def find_work_chains(self):
         builder = CalculationQueryBuilder()
         filters = builder.get_filters(
-            process_label="QeAppWorkChain",
+            process_label=self.process_label,
         )
         query_set = builder.get_query_set(
             filters=filters,
             order_by={"ctime": "desc"},
         )
+        projections = ["pk", "ctime", "state"]
         projected = builder.get_projected(
-            query_set, projections=["pk", "ctime", "state"]
+            query_set,
+            projections=projections,
         )
 
-        for process in projected[1:]:
-            workchain = load_node(process[0])
-            formula = workchain.inputs.structure.get_formula()
+        for result in projected[1:]:
+            process_info = dict(zip(projections, result))
 
-            properties = []
-            if "pdos" in workchain.inputs:
-                properties.append("pdos")
-            if "bands" in workchain.inputs:
-                properties.append("bands")
+            if self.EXTRA_FIELDS is not None:
+                pk = process_info["pk"]
+                extra_info = self.parse_extra_info(pk)
 
-            if "relax" in workchain.inputs:
-                builder_parameters = workchain.get_extra("builder_parameters", {})
-                relax_type = builder_parameters.get("relax_type")
-
-                if relax_type != "none":
-                    relax_info = "structure is relaxed"
-                else:
-                    relax_info = "static SCF calculation on structure"
+                yield make_dataclass("WorkChain", self.BASE_FIELDS + self.EXTRA_FIELDS)(
+                    **process_info, **extra_info
+                )
             else:
-                relax_info = "structure is not relaxed"
+                yield make_dataclass("WorkChain", self.BASE_FIELDS)(**process_info)
 
-            if not properties:
-                properties_info = ""
-            else:
-                properties_info = f"properties on {', '.join(properties)}"
-
-            yield cls.WorkChainData(
-                formula=formula,
-                relax_info=relax_info,
-                properties_info=properties_info,
-                *process,
-            )
-
-    @traitlets.default("busy")
+    @tl.default("busy")
     def _default_busy(self):
         return True
 
-    @traitlets.observe("busy")
+    @tl.observe("busy")
     def _observe_busy(self, change):
         for child in self.children:
             child.disabled = change["new"]
 
     def refresh_work_chains(self, _=None):
-        with self._refresh_lock:
-            try:
-                self.set_trait("busy", True)  # disables the widget
+        try:
+            self.set_trait("busy", True)  # disables the widget
 
-                with self.hold_trait_notifications():
-                    # We need to restore the original value, because it may be reset due to this issue:
-                    # https://github.com/jupyter-widgets/ipywidgets/issues/2230
-                    original_value = self.work_chains_selector.value
+            with self.hold_trait_notifications():
+                # We need to restore the original value, because it may be reset due to this issue:
+                # https://github.com/jupyter-widgets/ipywidgets/issues/2230
+                # It is fixed in ipywidgets 8.0.0, but we still support ipywidgets 7.x.
+                original_value = self.work_chains_selector.value
 
-                    self.work_chains_selector.options = [
-                        ("New workflow...", self._NO_PROCESS)
-                    ] + [
-                        (self.FMT_WORKCHAIN.format(wc=wc), wc.pk)
-                        for wc in self.find_work_chains()
-                    ]
+                self.work_chains_selector.options = [
+                    ("New workflow...", self._NO_PROCESS)
+                ] + [
+                    (self.FMT_WORKCHAIN.format(wc=wc), wc.pk)
+                    for wc in self.find_work_chains()
+                ]
 
-                    self.work_chains_selector.value = original_value
-            finally:
-                self.set_trait("busy", False)  # reenable the widget
+                self.work_chains_selector.value = original_value
+        finally:
+            self.set_trait("busy", False)  # reenable the widget
 
-    def _auto_refresh_loop(self):
-        while True:
-            self.refresh_work_chains()
-            if self._stop_refresh_thread.wait(timeout=self.auto_refresh_interval):
-                break
-
-    def _update_auto_refresh_thread_state(self):
-        if self.auto_refresh_interval > 0 and self._refresh_thread is None:
-            # start thread
-            self._stop_refresh_thread.clear()
-            self._refresh_thread = Thread(target=self._auto_refresh_loop)
-            self._refresh_thread.start()
-
-        elif self.auto_refresh_interval <= 0 and self._refresh_thread is not None:
-            # stop thread
-            self._stop_refresh_thread.set()
-            self._refresh_thread.join(timeout=30)
-            self._refresh_thread = None
-
-    @traitlets.default("auto_refresh_interval")
-    def _default_auto_refresh_interval(self):
-        return 10  # seconds
-
-    @traitlets.observe("auto_refresh_interval")
-    def _observe_auto_refresh_interval(self, change):
-        if change["new"] != change["old"]:
-            self._update_auto_refresh_thread_state()
-
-    @traitlets.observe("value")
+    @tl.observe("value")
     def _observe_value(self, change):
         if change["old"] == change["new"]:
             return
@@ -182,3 +126,42 @@ class WorkChainSelector(ipw.HBox):
             self.refresh_work_chains()
 
         self.work_chains_selector.value = new
+
+
+class QeAppWorkChainSelector(WorkChainSelector):
+    EXTRA_FIELDS = [("formula", str), ("relax_info", str), ("properties_info", str)]
+
+    def __init__(self, **kwargs):
+        super().__init__(process_label="QeAppWorkChain", **kwargs)
+
+    def parse_extra_info(self, uuid):
+        workchain = orm.load_node(uuid)
+        formula = workchain.inputs.structure.get_formula()
+
+        properties = []
+        if "pdos" in workchain.inputs:
+            properties.append("pdos")
+        if "bands" in workchain.inputs:
+            properties.append("bands")
+
+        if "relax" in workchain.inputs:
+            builder_parameters = workchain.get_extra("builder_parameters", {})
+            relax_type = builder_parameters.get("relax_type")
+
+            if relax_type != "none":
+                relax_info = "structure is relaxed"
+            else:
+                relax_info = "static SCF calculation on structure"
+        else:
+            relax_info = "structure is not relaxed"
+
+        if not properties:
+            properties_info = ""
+        else:
+            properties_info = f"properties on {', '.join(properties)}"
+
+        return {
+            "formula": formula,
+            "relax_info": relax_info,
+            "properties_info": properties_info,
+        }
