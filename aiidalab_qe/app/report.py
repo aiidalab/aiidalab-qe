793af07e91c962aa344c5a80f95c061db267807b
AndresOrtegaGuerrero <34098967+AndresOrtegaGuerrero@users.noreply.github.com>
2023-05-31 17:15:05 +0200

    Update aiidalab_qe/app/report.py
    Co-authored-by: Jusong Yu <jusong.yeu@gmail.com>

--- aiidalab_qe/app/report.py
+++ aiidalab_qe/app/report.py
@@ -65,7 +65,7 @@ def _generate_report_dict(builder_parameters: dict):
         yield "degauss", builder_parameters["degauss"]
         yield "smearing", builder_parameters["smearing"]
 
-    yield "tot_charge", builder_parameters.get("tot_charge", None)
+    yield "tot_charge", builder_parameters["tot_charge"]
 
 
 def _generate_report_html(report):
