46dd82fefbb225e193be000e91d1184f99f8e8a8
AndresOrtegaGuerrero <oandresg15@gmail.com>
2023-06-05 11:46:33 +0000

    DosBandsWidget plotly

--- aiidalab_qe/app/node_view.py
+++ aiidalab_qe/app/node_view.py
@@ -13,15 +13,17 @@ from tempfile import TemporaryDirectory
 
 import ipywidgets as ipw
 import nglview
+import numpy as np
 import traitlets
 from aiida.cmdline.utils.common import get_workchain_report
 from aiida.common import LinkType
 from aiida.orm import CalcJobNode, Node, ProjectionData, WorkChainNode
 from aiidalab_widgets_base import ProcessMonitor, register_viewer_widget
+from aiidalab_widgets_base.utils import string_range_to_list
 from aiidalab_widgets_base.viewers import StructureDataViewer
 from ase import Atoms
 from filelock import FileLock, Timeout
-from IPython.display import HTML, display
+from IPython.display import HTML, clear_output, display
 from jinja2 import Environment
 from monty.json import jsanitize
 from traitlets import Instance, Int, List, Unicode, Union, default, observe, validate
@@ -597,7 +599,8 @@ class WorkChainViewer(ipw.VBox):
             if "electronic_structure" not in self._results_shown and (
                 "band_structure" in self.node.outputs or "dos" in self.node.outputs
             ):
-                self._show_electronic_structure()
+                self._show_electronic_dosoptions()
+                # self._show_electronic_structure()
                 self._results_shown.add("electronic_structure")
 
     def _show_structure(self):
@@ -606,6 +609,11 @@ class WorkChainViewer(ipw.VBox):
         )
         self.result_tabs.children[1].children = [self._structure_view]
         self.result_tabs.set_title(1, "Final Geometry")
+
+    def _show_electronic_dosoptions(self):
+        self.dos_options = BandDosPlotsWidget(self.node)
+        self.result_tabs.children[2].children = [self.dos_options]
+        self.result_tabs.set_title(2, "Electronic Structure")
 
     def _show_electronic_structure(self):
         group_dos_by = ipw.ToggleButtons(
@@ -765,14 +773,11 @@ class BandDosPlotsWidget(ipw.VBox):
     )
 
     def __init__(self, node, **kwargs):
-        self.export_dir = Path.cwd().joinpath("exports")
-        self.dos_data = self._get_dos_data()
-        self.bands_data = self._get_bands_data()
-        self.fermi_energy = self._get_fermi_energy()
         self.node = node
+
         self.dos_atoms_group = ipw.Dropdown(
-            options=[("Atoms", "atoms"), ("Kinds", "kinds")],
-            value="atoms",
+            options=[("Kinds", "kinds"), ("Atoms", "atoms")],
+            value="kinds",
         )
         self.dos_plot_group = ipw.Dropdown(
             options=[("Total", "total"), ("Orbital", "orbital")],
@@ -801,6 +806,19 @@ class BandDosPlotsWidget(ipw.VBox):
             disabled=False,
             layout=ipw.Layout(width="auto", visibility="hidden"),
         )
+        self.export_dir = Path.cwd().joinpath("exports")
+        self.dos_data = self._get_dos_data()
+        self.fermi_energy = self._get_fermi_energy()
+        self.bands_data = self._get_bands_data()
+        self.band_labels = self.bands_labeling(self.bands_data)
+        self.bands_xaxis = self._band_xaxis()
+        self.bands_yaxis = self._band_yaxis()
+        self.dos_xaxis = self._dos_xaxis()
+        self.dos_yaxis = self._dos_yaxis()
+        self.bandsplot_widget = self._bandsplot_widget()
+
+        if self.bands_data and not self.dos_data:
+            self.update_plot_button.disabled = True
         self.bands_widget = ipw.Output()
 
         def download_data(_=None):
@@ -816,6 +834,8 @@ class BandDosPlotsWidget(ipw.VBox):
                 self._download(payload=b64_str, filename=file_name_dos)
 
         self.download_button.on_click(download_data)
+        self._initial_view()
+        self.update_plot_button.on_click(self._update_plot)
 
         super().__init__(
             children=[
@@ -873,29 +893,331 @@ class BandDosPlotsWidget(ipw.VBox):
         display(javas)
 
     def _get_dos_data(self):
-        if "pdos" in self.node.inputs.properties.get():
+        if "pdos" in self.node.inputs.properties:
+            expanded_selection, syntax_ok = string_range_to_list(
+                self.selected_atoms.value, shift=-1
+            )
             dos = get_pdos_data(
                 self.node,
                 group_tag=self.dos_atoms_group.value,
                 plot_tag=self.dos_plot_group.value,
-                selected_atoms=self.selected_atoms.value,
+                selected_atoms=expanded_selection,
             )
             return dos
         else:
             return None
 
     def _get_fermi_energy(self):
-        fermi_energy = self.dos["fermi_energy"] if self.dos else None
+        fermi_energy = self.dos_data["fermi_energy"] if self.dos_data else 0.0
         return fermi_energy
 
     def _get_bands_data(self):
-        if "bands" in self.node.inputs.properties.get():
+        if "bands" in self.node.inputs.properties:
             bands = export_bands_data(self.node, self.fermi_energy)
             return bands
         else:
             return None
 
-    def bands_labeling(bands):
+    def _initial_view(self):
+        with self.bands_widget:
+            clear_output(wait=True)
+            display(self.bandsplot_widget)
+            self.download_button.layout.visibility = "visible"
+
+    def _update_plot(self, _=None):
+        with self.bands_widget:
+            expanded_selection, syntax_ok = string_range_to_list(
+                self.selected_atoms.value, shift=-1
+            )
+            if not syntax_ok:
+                self.wrong_syntax.layout.visibility = "visible"
+                clear_output(wait=True)
+            else:
+                self.dos_data = self._get_dos_data()
+                self.bandsplot_widget = self._bandsplot_widget()
+                clear_output(wait=True)
+                display(self.bandsplot_widget)
+
+    def _bandsplot_widget(self):
+        import plotly.graph_objects as go
+        from plotly.subplots import make_subplots
+
+        if self.bands_data and not self.dos_data:
+            fig = go.Figure()
+            paths = self.bands_data[0].get("paths")
+
+            for band in paths:
+                for bands in band["values"]:
+                    bands_np = np.array(bands)
+                    fig.add_trace(
+                        go.Scatter(
+                            x=band["x"],
+                            y=bands_np - self.fermi_energy,  # substract Fermi Energy
+                            mode="lines",
+                            line=dict(color="#111111", shape="spline", smoothing=1.3),
+                            showlegend=False,
+                        )
+                    )
+            for i in self.band_labels[1]:
+                fig.add_vline(x=i, line=dict(color="#111111", width=1))
+            if self.fermi_energy != 0:
+                fig.add_hline(y=0, line=dict(color="#111111", width=1, dash="dot"))
+            fig.update_layout(
+                height=600,
+                width=950,
+                plot_bgcolor="white",
+                xaxis=self.bands_xaxis,
+                yaxis=self.bands_yaxis,
+            )
+
+        elif self.dos_data and not self.bands_data:
+            fig = go.Figure()
+            for trace in self.dos_data["dos"]:
+                if trace["label"] == "Total DOS":
+                    my_fill = "tozeroy"
+                else:
+                    my_fill = None
+                dos_np = np.array(trace["x"])
+                fig.add_trace(
+                    go.Scatter(
+                        x=dos_np - self.fermi_energy,
+                        y=trace["y"],
+                        fill=my_fill,
+                        name=trace["label"],
+                        line=dict(
+                            color=trace["borderColor"], shape="spline", smoothing=1.0
+                        ),
+                    )
+                )
+            fig.add_vline(x=0, line=dict(color="#111111", width=1, dash="dot"))
+            fig.update_layout(
+                height=600,
+                width=950,
+                plot_bgcolor="white",
+                xaxis=self.dos_xaxis,
+                yaxis=self.dos_yaxis,
+            )
+
+        elif self.dos_data and self.bands_data:
+            fig = make_subplots(
+                rows=1,
+                cols=2,
+                shared_yaxes=True,
+                column_widths=[0.7, 0.3],
+                horizontal_spacing=0.02,
+            )
+            paths = self.bands_data[0].get("paths")
+            for band in paths:
+                for bands in band["values"]:
+                    bands_np = np.array(bands)
+                    fig.add_trace(
+                        go.Scatter(
+                            x=band["x"],
+                            y=bands_np - self.fermi_energy,  # substract Fermi Energy
+                            mode="lines",
+                            line=dict(color="#111111", shape="spline", smoothing=1.3),
+                            showlegend=False,
+                        ),
+                        row=1,
+                        col=1,
+                    )
+
+            for trace in self.dos_data["dos"]:
+                if trace["label"] == "Total DOS":
+                    my_fill = "tozerox"
+                else:
+                    my_fill = None
+
+                dos_np = np.array(trace["x"])
+                fig.add_trace(
+                    go.Scatter(
+                        x=trace["y"],
+                        y=dos_np - self.fermi_energy,
+                        fill=my_fill,
+                        name=trace["label"],
+                        line=dict(
+                            color=trace["borderColor"], shape="spline", smoothing=1.3
+                        ),
+                    ),
+                    row=1,
+                    col=2,
+                )
+            for i in self.band_labels[1]:
+                fig.add_vline(
+                    x=i,
+                    line=dict(color="#111111", width=1),
+                    row=1,
+                    col=1,
+                )
+            fig.update_xaxes(
+                patch=self.bands_xaxis,
+                row=1,
+                col=1,
+            )
+            fig.update_yaxes(
+                patch=self.bands_yaxis,
+                row=1,
+                col=1,
+            )
+            fig.update_xaxes(
+                patch=self.dos_xaxis,
+                row=1,
+                col=2,
+            )
+            fig.update_yaxes(patch=self.dos_yaxis, row=1, col=2, showticklabels=False)
+
+            fig.add_hline(
+                y=0, line=dict(color="#111111", width=1, dash="dot"), row=1, col=1
+            )
+            fig.add_hline(
+                y=0, line=dict(color="#111111", width=1, dash="dot"), row=1, col=2
+            )
+            fig.update_layout(
+                height=600,
+                width=850,
+                plot_bgcolor="white",
+                legend=dict(
+                    # yanchor="top",
+                    # y=0.99,
+                    xanchor="left",
+                    x=1.04,
+                ),
+            )
+
+        else:
+            fig = None
+
+        return go.FigureWidget(fig)
+
+    def _band_xaxis(self):
+        import plotly.graph_objects as go
+
+        if self.bands_data:
+            paths = self.bands_data[0].get("paths")
+            # labels = bands_labeling(self.bands_data)
+            # labels, labels_values = bands_labeling(self.bands_data)
+            slider_bands = go.layout.xaxis.Rangeslider(
+                thickness=0.08,
+                range=[0, paths[-1]["x"][-1]],
+            )
+
+            bandxaxis = go.layout.XAxis(
+                title="k-points",
+                range=[0, paths[-1]["x"][-1]],
+                showgrid=True,
+                showline=True,
+                tickmode="array",
+                rangeslider=slider_bands,
+                fixedrange=False,
+                tickvals=self.band_labels[1],
+                ticktext=self.band_labels[0],
+                showticklabels=True,
+                linecolor="#111111",
+                mirror=True,
+                linewidth=2,
+                type="linear",
+            )
+
+        else:
+            bandxaxis = None
+        return bandxaxis
+
+    def _band_yaxis(self):
+        import plotly.graph_objects as go
+
+        if self.bands_data:
+            bandyaxis = go.layout.YAxis(
+                # title="Electronic Bands(eV)",
+                title=dict(text="Electronic Bands (eV)", standoff=1),
+                side="left",
+                showgrid=True,
+                showline=True,
+                zeroline=True,
+                fixedrange=False,
+                automargin=True,
+                mirror="ticks",
+                ticks="inside",
+                linewidth=2,
+                linecolor="#111111",
+                tickwidth=2,
+                zerolinewidth=2,
+            )
+
+        else:
+            bandyaxis = None
+        return bandyaxis
+
+    def _dos_xaxis(self):
+        import plotly.graph_objects as go
+
+        if self.dos_data:
+            if self.bands_data:
+                dosxaxis = go.layout.XAxis(
+                    title="Density of states",
+                    side="bottom",
+                    showgrid=True,
+                    showline=True,
+                    linecolor="#111111",
+                    mirror="ticks",
+                    ticks="inside",
+                    linewidth=2,
+                    tickwidth=2,
+                )
+
+            else:
+                dosxaxis = go.layout.XAxis(
+                    title="Density of states (eV)",
+                    showgrid=True,
+                    showline=True,
+                    linecolor="#111111",
+                    mirror="ticks",
+                    ticks="inside",
+                    linewidth=2,
+                    tickwidth=2,
+                )
+
+        else:
+            dosxaxis = None
+        return dosxaxis
+
+    def _dos_yaxis(self):
+        import plotly.graph_objects as go
+
+        if self.dos_data:
+            if self.bands_data:
+                dosyaxis = go.layout.YAxis(
+                    # title="Density of states (eV)", #FigureWidget modifies the title position in the meantime lets put it
+                    showgrid=True,
+                    showline=True,
+                    side="right",
+                    # position=0.0,
+                    mirror="ticks",
+                    ticks="inside",
+                    linewidth=2,
+                    tickwidth=2,
+                    linecolor="#111111",
+                    zerolinewidth=2,
+                )
+
+            else:
+                dosyaxis = go.layout.YAxis(
+                    # title="Density of states (eV)",
+                    showgrid=True,
+                    showline=True,
+                    side="left",
+                    mirror="ticks",
+                    ticks="inside",
+                    linewidth=2,
+                    tickwidth=2,
+                    linecolor="#111111",
+                    zerolinewidth=2,
+                )
+
+        else:
+            dosyaxis = None
+        return dosyaxis
+
+    def bands_labeling(self, bands):
         """Function to return two list containing the labels and values (kpoint) for plotting
         params: bands: dictionary from export_bands_data function
         output: label (list of str), label_values (list of float)
@@ -923,7 +1245,7 @@ class BandDosPlotsWidget(ipw.VBox):
 
         labels = [label[0] for label in my_clean_labels]
         labels_values = [label[1] for label in my_clean_labels]
-        return labels, labels_values
+        return [labels, labels_values]
 
 
 def get_pdos_data(work_chain_node, group_tag, plot_tag, selected_atoms):
