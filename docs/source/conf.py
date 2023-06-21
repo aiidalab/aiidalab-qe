b0df162693de4113dc4798267f6d3139aa047029
Edan Bainglass <edan.bainglass@gmail.com>
2023-06-21 11:04:51 +0200

    Change image to PNG (still not great)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,7 +77,7 @@ html_theme = "pydata_sphinx_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/images/full_logo.svg"
+html_logo = "_static/images/full_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
