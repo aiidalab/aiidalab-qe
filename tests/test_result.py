d885ad22d6b5a3e3cc2e20f2e51ac7689df412e1
superstar54 <xingwang1991@gmail.com>
2023-06-29 10:33:35 +0000

    update test for result

--- tests/test_result.py
+++ tests/test_result.py
@@ -4,7 +4,7 @@ def test_result_step(app_to_submit, generate_qeapp_workchain):
 
     step = app_to_submit.steps.steps[3][1]
     step.process = generate_qeapp_workchain().node.uuid
-    assert step.state == step.State.SUCCESS
+    assert step.state == step.State.ACTIVE
 
 
 def test_workchainview(generate_qeapp_workchain):
@@ -26,7 +26,10 @@ def test_summary_view(generate_qeapp_workchain):
     wcv = WorkChainViewer(wkchain.node)
     print(wcv.result_tabs.children[0].children[0].children[0])
     # assert "None" not in report_html
-    assert "None" not in wcv.result_tabs.children[0].children[0].children[0].value
+    assert (
+        "Workflow Summary not available."
+        in wcv.result_tabs.children[0].children[0].children[0].children[0].value
+    )
 
 
 def test_electronic_structure(generate_qeapp_workchain):
@@ -45,9 +48,14 @@ def test_electronic_structure(generate_qeapp_workchain):
 def test_workchain_outputs(generate_qeapp_workchain):
     """Test the workchain outputs widget can be properly generated
     without errors"""
+    from aiida import engine
+
     from aiidalab_qe.app.result.node_view import WorkChainOutputs
 
     wkchain = generate_qeapp_workchain()
-    widget = WorkChainOutputs(wkchain.node)
+    qeapp_node = wkchain.node
+    qeapp_node.set_exit_status(0)
+    qeapp_node.set_process_state(engine.ProcessState.FINISHED)
+    widget = WorkChainOutputs(qeapp_node)
     widget._download_archive(1)
     # TODO: check the content of the archive
