9e2aa3c51e11df55a191a6be884742dfad64b5ed
Jusong Yu <jusong.yeu@gmail.com>
2023-07-31 11:11:22 +0200

    Bump version v23.04.5 -> v23.04.6

--- aiidalab_qe/version.py
+++ aiidalab_qe/version.py
@@ -3,4 +3,4 @@
 
 """This module contains project version information."""
 
-__version__ = "v23.04.5"
+__version__ = "v23.04.6"
--- src/aiidalab_qe_workchain/__init__.py
+++ src/aiidalab_qe_workchain/__init__.py
@@ -392,4 +392,4 @@ class QeAppWorkChain(WorkChain):
             )
 
 
-__version__ = "23.4.5"
+__version__ = "23.4.6"
