504725346f9bff08afef2510b08c1e493da3d904
Jusong Yu <jusong.yeu@gmail.com>
2023-08-25 14:51:03 +0200

    Using newly baked container for integration tests

--- tests_integration/test_qe_app.py
+++ tests_integration/test_qe_app.py
@@ -13,6 +13,14 @@ def test_notebook_service_available(notebook_service):
     assert response.status_code == 200
 
 
+def test_verdi_status(aiidalab_exec, nb_user, notebook_service):
+    # Check the aiida service is running and connected to RabbitMQ
+    # The notebook_service fixture is needed to wait for the services to be up
+    output = aiidalab_exec("verdi status", user=nb_user).decode().strip()
+    assert "Connected to RabbitMQ" in output
+    assert "Daemon is running" in output
+
+
 def test_qe_app_take_screenshot(selenium_driver, final_screenshot):
     driver = selenium_driver("qe.ipynb", wait_time=30.0)
     driver.set_window_size(1920, 1485)
