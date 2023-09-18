a821ef4c43a8f0c01f95f2bf06e39a3c980af892
superstar54 <xingwang1991@gmail.com>
2023-09-18 15:12:52 +0000

    update path

--- src/aiidalab_qe/app/structure/__init__.py
+++ src/aiidalab_qe/app/structure/__init__.py
@@ -3,7 +3,6 @@
 Authors: AiiDAlab team
 """
 import os
-import pathlib
 import warnings
 
 import aiida
@@ -28,8 +27,7 @@ structures are supported.
 </div>"""
 
 # Navigate to the aiidalab_qe root directory
-file_path = pathlib.Path(__file__)
-file_path = os.path.abspath(os.path.join(file_path, "..", "..", "..", "..", "../"))
+file_path = os.path.abspath(os.path.join(__file__, "../../../../../"))
 # The Examples list of (name, file) tuple curretly passed to
 # StructureExamplesWidget.
 Examples = [
