139328a3cc5dcb0fdcf6512117d0d36cc1a9d87f
superstar54 <xingwang1991@gmail.com>
2023-09-20 12:22:10 +0000

    udpate teset

--- tests_notebooks/test_qe_app.py
+++ tests_notebooks/test_qe_app.py
@@ -35,6 +35,10 @@ def test_qe_app_select_silicon_and_confirm(
     try:
         driver.find_element(By.XPATH, "//option[@value='Diamond']").click()
         time.sleep(10)
+        element = WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
+        )
+        element.click()
     except Exception as exception:
         print(exception)
         driver.find_element(By.TAG_NAME, "summary").click()
@@ -43,10 +47,5 @@ def test_qe_app_select_silicon_and_confirm(
     driver.get_screenshot_as_file(
         str(Path.joinpath(screenshot_dir, "qe-app-select-diamond-selected.png"))
     )
-    element = WebDriverWait(driver, 60).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
-    )
-    element.click()
-
     # Test that we have indeed proceeded to the next step
     driver.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
