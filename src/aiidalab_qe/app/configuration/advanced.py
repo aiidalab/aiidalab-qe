06bcc9dc729355ca6d239a982c62ebfc32db33cc
AndresOrtegaGuerrero <34098967+AndresOrtegaGuerrero@users.noreply.github.com>
2023-09-20 14:06:22 +0200

    adding HTML to visualise the kpoints mesh (#480)

--- src/aiidalab_qe/app/configuration/advanced.py
+++ src/aiidalab_qe/app/configuration/advanced.py
@@ -8,6 +8,9 @@ import os
 import ipywidgets as ipw
 import traitlets as tl
 from aiida import orm
+from aiida_quantumespresso.calculations.functions.create_kpoints_from_distance import (
+    create_kpoints_from_distance,
+)
 from aiida_quantumespresso.workflows.pw.base import PwBaseWorkChain
 from IPython.display import clear_output, display
 
@@ -70,12 +73,14 @@ class AdvancedSettings(Panel):
         )
 
         # Kpoints setting widget
-        self.kpoints_distance = ipw.FloatText(
+        self.kpoints_distance = ipw.BoundedFloatText(
+            min=0.0,
             step=0.05,
             description="K-points distance (1/Å):",
             disabled=False,
             style={"description_width": "initial"},
         )
+        self.mesh_grid = ipw.HTML()
         ipw.dlink(
             (self.override, "value"),
             (self.kpoints_distance, "disabled"),
@@ -111,6 +116,7 @@ class AdvancedSettings(Panel):
             (self.pseudo_family_selector, "value"),
             (self.pseudo_setter, "pseudo_family"),
         )
+        self.kpoints_distance.observe(self._display_mesh, "value")
         self.children = [
             self.title,
             ipw.HBox(
@@ -125,7 +131,7 @@ class AdvancedSettings(Panel):
             self.smearing,
             # Kpoints setting widget
             self.kpoints_description,
-            self.kpoints_distance,
+            ipw.HBox([self.kpoints_distance, self.mesh_grid]),
             self.pseudo_family_selector,
             self.pseudo_setter,
         ]
@@ -148,6 +154,7 @@ class AdvancedSettings(Panel):
         if self.input_structure is not None:
             self.magnetization._update_widget(change)
             self.pseudo_setter.structure = change["new"]
+            self._display_mesh()
 
     @tl.observe("protocol")
     def _protocol_changed(self, _):
@@ -260,6 +267,19 @@ class AdvancedSettings(Panel):
 
             # reset the override checkbox
             self.override.value = False
+
+    def _display_mesh(self, _=None):
+        if self.input_structure is None:
+            return
+        if self.kpoints_distance.value > 0:
+            mesh = create_kpoints_from_distance(
+                self.input_structure,
+                orm.Float(self.kpoints_distance.value),
+                orm.Bool(True),
+            )
+            self.mesh_grid.value = "Mesh " + str(mesh.get_kpoints_mesh()[0])
+        else:
+            self.mesh_grid.value = "Please select a number higher than 0.0"
 
 
 class MagnetizationSettings(ipw.VBox):
