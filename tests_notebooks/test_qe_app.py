658882b3b9e2f6a436f06e012fb6cc1021b715cc
Xing Wang <xingwang1991@gmail.com>
2023-09-21 11:31:05 +0200

    Add unit test for workchain (#472)
    This commit adds tests for the `QeAppWorkChain` which test the workchain widget and behavior as a whole. 
    
    It also includes the test for plugins: `bands` and `pdos`.
    The test generates the projection data and bands data, then mounts them to the `QeAppWorkChain`. The final result is then checked.

--- tests_notebooks/test_qe_app.py
+++ tests_notebooks/test_qe_app.py
@@ -32,17 +32,19 @@ def test_qe_app_select_silicon_and_confirm(
     )
     element.click()
 
-    driver.find_element(By.XPATH, "//option[@value='Diamond']").click()
-    time.sleep(10)
-
+    try:
+        driver.find_element(By.XPATH, "//option[@value='Diamond']").click()
+        time.sleep(10)
+        element = WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
+        )
+        element.click()
+    except Exception:
+        driver.find_element(By.TAG_NAME, "summary").click()
+        time.sleep(10)
+    # Take a screenshot of the selected diamond
     driver.get_screenshot_as_file(
         str(Path.joinpath(screenshot_dir, "qe-app-select-diamond-selected.png"))
     )
-
-    element = WebDriverWait(driver, 60).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
-    )
-    element.click()
-
     # Test that we have indeed proceeded to the next step
     driver.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
