e15086ef4144fc2cea672c76279e915d21de6638
superstar54 <xingwang1991@gmail.com>
2023-09-26 12:52:52 +0000

    add test for reset

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
-def test_reload(submit_app_generator, generate_qeapp_workchain):
-    """Test if the GUI paramters can be reload properly"""
+def test_reload_and_reset(submit_app_generator, generate_qeapp_workchain):
+    """Test if the GUI paramters can be reload and reset properly"""
     wkchain = generate_qeapp_workchain(
         relax_type="positions", run_bands=True, run_pdos=False, spin_type="collinear"
     )
@@ -10,4 +10,11 @@ def test_reload(submit_app_generator, generate_qeapp_workchain):
     assert app.configure_step.workchain_settings.relax_type.value == "positions"
     assert app.configure_step.workchain_settings.spin_type.value == "collinear"
     assert app.configure_step.workchain_settings.properties["bands"].run.value is True
+    assert app.configure_step.workchain_settings.properties["pdos"].run.value is False
+    # new workflow, this will reset the GUI
+    app.work_chain_selector.value = None
+    # check if the value are reload correctly
+    assert app.configure_step.workchain_settings.relax_type.value == "positions_cell"
+    assert app.configure_step.workchain_settings.spin_type.value == "none"
+    assert app.configure_step.workchain_settings.properties["bands"].run.value is False
     assert app.configure_step.workchain_settings.properties["pdos"].run.value is False
