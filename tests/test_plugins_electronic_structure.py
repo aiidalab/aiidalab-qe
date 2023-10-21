761293f86f3b42293ad0e0fbe646e961bdba3d6c
superstar54 <xingwang1991@gmail.com>
2023-10-21 06:18:38 +0000

    add `electronic_structure` plugin

---
+++ tests/test_plugins_electronic_structure.py
@@ -0,0 +1,18 @@
+def test_electronic_structure(generate_qeapp_workchain):
+    """test the report can be properly generated from the builder without errors"""
+    from aiida import engine
+
+    from aiidalab_qe.app.result.workchain_viewer import WorkChainViewer
+
+    wkchain = generate_qeapp_workchain()
+    wkchain.node.set_exit_status(0)
+    wkchain.node.set_process_state(engine.ProcessState.FINISHED)
+    wcv = WorkChainViewer(wkchain.node)
+    # find the tab with the identifier "electronic_structure"
+    tab = [
+        tab
+        for tab in wcv.result_tabs.children[2:]
+        if tab.identifier == "electronic_structure"
+    ][0]
+    # check the content of the tab
+    assert "DOS grouped by:" == tab.children[0].children[0].children[0].value
