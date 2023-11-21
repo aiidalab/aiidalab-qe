aefb0adc25c52eb2d51d404368a6622dcbd80f2a
superstar54 <xingwang1991@gmail.com>
2023-11-21 22:58:32 +0000

    monitor trajectory

--- src/aiidalab_qe/plugins/pw_monitor/result.py
+++ src/aiidalab_qe/plugins/pw_monitor/result.py
@@ -26,10 +26,15 @@ class Result(ResultPanel):
         """Draw the SCF convergence plot."""
         import time
 
+        import nglview as nv
         import plotly.graph_objects as go
 
         #
         def get_data(relax_node):
+            """Get the data from the extras of the relax node."""
+            from ase import Atoms
+
+            images = []
             energies = []
             pw_base_nodes = [
                 link.node
@@ -41,7 +46,17 @@ class Result(ResultPanel):
                     if not link.link_label.startswith("iteration"):
                         continue
                     energies.extend(link.node.base.extras.get("monitor_energies", []))
-            return energies
+                    atoms = link.node.base.extras.get("monitor_atoms", {})
+                    trajectory = link.node.base.extras.get("monitor_trajectory", [])
+                    for i in range(len(trajectory)):
+                        atoms.update(
+                            {"positions": trajectory[i], "pbc": [True, True, True]}
+                        )
+                        images.append(Atoms(**atoms))
+            # the first image is the initial structure
+            if len(images) == 0:
+                images = [relax_node.inputs.structure.get_ase()]
+            return energies, images
 
         #
         if "relax" not in self.node.base.links.get_outgoing().all_link_labels():
@@ -51,22 +66,47 @@ class Result(ResultPanel):
         relax_node = self.node.base.links.get_outgoing().get_node_by_label("relax")
         g = go.FigureWidget(
             layout=go.Layout(
-                title=dict(text="SCF convergence"),
+                title=dict(text="SCF iteration."),
                 barmode="overlay",
             )
         )
         g.layout.xaxis.title = "SCF step"
         g.layout.yaxis.title = "Energy (eV)"
-        energies = get_data(relax_node)
+        energies, images = get_data(relax_node)
         g.add_scatter(x=list(range(len(energies))), y=energies, mode="lines+markers")
-        self.children = (ipw.HTML("<h4>PW calculation running!</h4>"), g)
+        atoms_viewer = nv.show_asetraj(images)
+        atoms_viewer.frame = len(images) - 1
+        self.children = (
+            ipw.HTML("<h4>PW calculation is running!</h4>"),
+            g,
+            ipw.HTML("<h4>Trajectory</h4>"),
+            atoms_viewer,
+        )
+        nimage = len(images)
         while relax_node.process_state.value not in ["finished", "excepted", "killed"]:
-            energies = get_data(relax_node)
+            energies, images = get_data(relax_node)
+            # udpate the atoms viewer if there are new images
+            if len(images) > nimage:
+                atoms_viewer = nv.show_asetraj(images)
+                atoms_viewer.frame = len(images) - 1
+                self.children = (
+                    ipw.HTML("<h4>PW calculation is running!</h4>"),
+                    g,
+                    ipw.HTML("<h4>Trajectory</h4>"),
+                    atoms_viewer,
+                )
+                nimage = len(images)
             with g.batch_update():
                 g.data[0].x = list(range(len(energies)))
                 g.data[0].y = energies
             time.sleep(update_interval)
-        self.children = (ipw.HTML("<h4>PW calculation completed!</h4>"), g)
+        state = relax_node.process_state.value
+        self.children = (
+            ipw.HTML(f"<h4>PW calculation is {state}!</h4>"),
+            g,
+            ipw.HTML("<h4>Trajectory</h4>"),
+            atoms_viewer,
+        )
 
     def reset(self):
         """Reset the widget.
--- src/aiidalab_qe/workflows/monitor.py
+++ src/aiidalab_qe/workflows/monitor.py
@@ -11,12 +11,29 @@ def relax_pw(node: CalcJobNode, transport: Transport) -> str:
     :param transport: The transport that can be used to retrieve files from remote working directory.
     :returns: A string if the job should be killed, `None` otherwise.
     """
+    import numpy as np
+    from ase.io import read
+
     try:
+        # read structure
+        with tempfile.NamedTemporaryFile("w+") as handle:
+            transport.getfile(node.get_option("output_filename"), handle.name)
+            handle.seek(0)
+            images = read(handle, index=":", format="espresso-out")
+            if len(images) > 0:
+                atoms = images[0].todict()
+                # type `<class 'numpy.bool_'>` is not supported as it is not json-serializable
+                atoms.pop("pbc")
+                trajectory = np.zeros((len(images), len(images[0]), 3))
+                for i in range(len(images)):
+                    trajectory[i] = images[i].positions
+                node.base.extras.set("monitor_atoms", atoms)
+                node.base.extras.set("monitor_trajectory", trajectory)
+        # read scf energy
         with tempfile.NamedTemporaryFile("w+") as handle:
             transport.getfile(node.get_option("output_filename"), handle.name)
             handle.seek(0)
             output = handle.read()
-
         energies = []
         lines = output.splitlines()
         for line in lines:
