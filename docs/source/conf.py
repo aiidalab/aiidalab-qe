d70ad697e468f5f7d2ed62869963a7a51daa818c
Jusong Yu <jusong.yeu@gmail.com>
2024-01-23 01:48:42 +0100

    Bump version v23.10.0 -> v23.10.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import time
 
 # -- Project information -----------------------------------------------------
 
-version = "v23.10.0"
+version = "v23.10.1"
 release = f"{version}-dev"
 project = "Quantum ESPRESSO App"
 copyright_first_year = "2023"
