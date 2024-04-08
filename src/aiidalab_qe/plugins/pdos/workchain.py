8da3db97e0be6451c2f9d4f5b04780f9e31b825f
Xing Wang <xingwang1991@gmail.com>
2024-04-08 17:07:07 +0200

    assgin clean_workdir to plugin's builder (#667)
    * assgin clean_workdir to plugin's builder
    * check if the plugin has a clean_workdir input

--- src/aiidalab_qe/plugins/pdos/workchain.py
+++ src/aiidalab_qe/plugins/pdos/workchain.py
@@ -80,6 +80,6 @@ def get_builder(codes, structure, parameters, **kwargs):
 
 workchain_and_builder = {
     "workchain": PdosWorkChain,
-    "exclude": ("clean_workdir", "structure", "relax"),
+    "exclude": ("structure", "relax"),
     "get_builder": get_builder,
 }
