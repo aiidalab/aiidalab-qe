ce72b686c163619bf7a03c0ff337bbbb5f5231db
superstar54 <xingwang1991@gmail.com>
2024-05-06 08:23:50 +0000

    Bump version v24.04.0rc3 -> v24.04.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import time
 
 # -- Project information -----------------------------------------------------
 
-version = "v24.04.0rc3"
+version = "v24.04.0"
 release = f"{version}-dev"
 project = "Quantum ESPRESSO App"
 copyright_first_year = "2023"
