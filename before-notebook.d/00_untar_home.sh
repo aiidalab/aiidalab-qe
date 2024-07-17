46bbe7177b77b3832ac5b4f8a93ed7ccb63e9993
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-07-17 23:44:46 +0100

    Gzipped

--- before-notebook.d/00_untar_home.sh
+++ before-notebook.d/00_untar_home.sh
@@ -9,9 +9,9 @@ fi
 
 # Untar home archive file to restore home directory if it is empty
 if [[ $(ls -A ${home} | wc -l) = "0" ]];then
-  if [ -f /opt/home.tar ]; then
+  if [ -f /opt/home.tar.gz ]; then
     # untar /opt/home.tar to /home/jovyan to restore home directory
     echo "Untarring /opt/home.tar to /home/jovyan"
-    tar -xf /opt/home.tar -C /home/jovyan
+    tar -xf /opt/home.tar.gz -C /home/${NB_USER}
   fi
 fi
