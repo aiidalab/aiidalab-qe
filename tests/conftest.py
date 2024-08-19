8855969f0f1703c76de574a806a874bbb90ad5b9
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-08-19 14:45:08 +0100

    Code/Pseudos setup: Separate frontend from backend (#796)
    * Document docker images in README

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pathlib
 import tempfile
 
 import pytest
-from aiidalab_qe.common.setup_pseudos import SSSP_VERSION
+from aiidalab_qe.setup.pseudos import SSSP_VERSION
 
 from aiida import orm
 
