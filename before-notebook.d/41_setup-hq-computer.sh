eb79d2a6c6487d8e8dbb7c9a1c2a35e784259333
Jusong Yu <jusong.yeu@gmail.com>
2024-09-17 09:45:19 +0200

    f-d: no if-else need to check if the computer already set

--- before-notebook.d/41_setup-hq-computer.sh
+++ before-notebook.d/41_setup-hq-computer.sh
@@ -2,25 +2,20 @@
 
 set -x
 
-computer_list=$(verdi computer list)
-if echo ${computer_list} | grep -q ${HQ_COMPUTER}; then
-    echo "${HQ_COMPUTER} already setup"
-else
-    # computer
-    verdi computer show ${HQ_COMPUTER} || verdi computer setup        \
-      --non-interactive                                               \
-      --label "${HQ_COMPUTER}"                                        \
-      --description "local computer with hyperqueue scheduler"        \
-      --hostname "localhost"                                          \
-      --transport core.local                                          \
-      --scheduler hyperqueue                                          \
-      --work-dir /home/${NB_USER}/aiida_run/                          \
-      --mpirun-command "mpirun -np {num_cpus}"
+# computer
+verdi computer show ${HQ_COMPUTER} || verdi computer setup        \
+  --non-interactive                                               \
+  --label "${HQ_COMPUTER}"                                        \
+  --description "local computer with hyperqueue scheduler"        \
+  --hostname "localhost"                                          \
+  --transport core.local                                          \
+  --scheduler hyperqueue                                          \
+  --work-dir /home/${NB_USER}/aiida_run/                          \
+  --mpirun-command "mpirun -np {num_cpus}"
 
-    verdi computer configure core.local "${HQ_COMPUTER}"              \
-      --non-interactive                                               \
-      --safe-interval 5.0
+verdi computer configure core.local "${HQ_COMPUTER}"              \
+  --non-interactive                                               \
+  --safe-interval 5.0
 
-    # disable the localhost which is set in base image
-    verdi computer disable localhost aiida@localhost
-fi
+# disable the localhost which is set in base image
+verdi computer disable localhost aiida@localhost
