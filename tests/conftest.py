6c7d6b6fb6cf4aa774601e94af468e398c03ddca
AndresOrtegaGuerrero <34098967+AndresOrtegaGuerrero@users.noreply.github.com>
2024-09-24 14:54:33 +0200

    Electron MaxStep Widget (#800)
    * Electron MaxStep Widget

--- tests/conftest.py
+++ tests/conftest.py
@@ -357,6 +357,7 @@ def submit_app_generator(
         tot_charge=0.0,
         vdw_corr="none",
         initial_magnetic_moments=0.0,
+        electron_maxstep=80,
     ):
         configure_step = app.configure_step
         # Settings
@@ -379,6 +380,7 @@ def submit_app_generator(
         configure_step.advanced_settings.magnetization._set_magnetization_values(
             initial_magnetic_moments
         )
+        configure_step.advanced_settings.electron_maxstep.value = electron_maxstep
         # mimic the behavior of the smearing widget set up
         configure_step.advanced_settings.smearing.smearing.value = smearing
         configure_step.advanced_settings.smearing.degauss.value = degauss
