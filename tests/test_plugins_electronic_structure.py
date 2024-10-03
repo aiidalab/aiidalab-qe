74562529874efa6c3c126baa22fc8c3eea8719c4
AndresOrtegaGuerrero <oandresg15@gmail.com>
2024-10-03 14:27:31 +0000

    update len of electronic strucutre test

--- tests/test_plugins_electronic_structure.py
+++ tests/test_plugins_electronic_structure.py
@@ -20,7 +20,7 @@ def test_electronic_structure(generate_qeapp_workchain):
         if getattr(tab, "identifier", "") == "electronic_structure"
     )
     # It should have one children: the _bands_plot_view
-    assert len(tab.children) == 1
+    assert len(tab.children) == 2
 
     result = Result(node=wkchain.node)
     result._update_view()
