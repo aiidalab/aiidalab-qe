6ef3b0f92ab63626d5c3a1666b94a5731192ddf8
AndresOrtegaGuerrero <oandresg15@gmail.com>
2024-11-28 14:19:16 +0000

    adding spin-orbit coupling to process label

--- src/aiidalab_qe/app/submission/model.py
+++ src/aiidalab_qe/app/submission/model.py
@@ -98,6 +98,16 @@ class SubmissionStepModel(
             "workchain",
             {"properties": []},
         )
+
+        soc_parameters = self.input_parameters["advanced"]["pw"]["parameters"][
+            "SYSTEM"
+        ].get("lspinorb", False)
+
+        if soc_parameters:
+            soc_info = ", spin-orbit coupling"
+        else:
+            soc_info = ""
+
         properties = [p for p in workchain_data["properties"] if p != "relax"]
         relax_type = workchain_data.get("relax_type", "none")
         relax_info = "unrelaxed"
@@ -109,7 +119,7 @@ class SubmissionStepModel(
         if workchain_data["spin_type"] != "none":
             protocol_and_magnetic_info += ", magnetic"
         properties_info = f"→ {', '.join(properties)}" if properties else ""
-        label = f"{structure_label} [{relax_info}, {protocol_and_magnetic_info}] {properties_info}".strip()
+        label = f"{structure_label} [{relax_info}, {protocol_and_magnetic_info}{soc_info}] {properties_info}".strip()
         self.process_label = label
 
     def update_submission_blockers(self):
