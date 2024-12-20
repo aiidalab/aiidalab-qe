2fe12117be909a2a77819035f48d694f03469fe4
Edan Bainglass <edan.bainglass@gmail.com>
2024-12-20 09:59:43 +0000

    Clean up app wrapper MVC

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -2,19 +2,19 @@ from aiidalab_qe.app.wrapper import AppWrapperContoller, AppWrapperModel, AppWra
 
 
 class TestWrapper:
-    def test_enable_toggles(self):
-        """Test enable_toggles method."""
+    def test_enable_controls(self):
+        """Test enable_controls method."""
         self._instansiate_mvc_components()
         assert self.view.guide_toggle.disabled is True
         assert self.view.about_toggle.disabled is True
-        self.controller.enable_toggles()
+        self.controller.enable_controls()
         assert self.view.guide_toggle.disabled is False
         assert self.view.about_toggle.disabled is False
 
     def test_guide_toggle(self):
         """Test guide_toggle method."""
         self._instansiate_mvc_components()
-        self.controller.enable_toggles()
+        self.controller.enable_controls()
         self.controller._on_guide_toggle({"new": True})
         self._assert_guide_is_on()
         self.controller._on_guide_toggle({"new": False})
@@ -23,7 +23,7 @@ class TestWrapper:
     def test_about_toggle(self):
         """Test about_toggle method."""
         self._instansiate_mvc_components()
-        self.controller.enable_toggles()
+        self.controller.enable_controls()
         self.controller._on_about_toggle({"new": True})
         self._assert_about_is_on()
         self.controller._on_about_toggle({"new": False})
@@ -32,7 +32,7 @@ class TestWrapper:
     def test_toggle_switch(self):
         """Test toggle_switch method."""
         self._instansiate_mvc_components()
-        self.controller.enable_toggles()
+        self.controller.enable_controls()
         self._assert_no_info()
         self.controller._on_guide_toggle({"new": True})
         self._assert_guide_is_on()
