a25136205ea58459a594e2443cbb1d79b0224174
Edan Bainglass <edan.bainglass@gmail.com>
2024-12-20 09:35:16 +0000

    Set default `on_demo_server` return value to False

--- src/aiidalab_qe/config/environment.py
+++ src/aiidalab_qe/config/environment.py
@@ -10,3 +10,4 @@ def on_demo_server():
         with CONFIG_PATH.open("r") as file:
             config = yaml.safe_load(file)
             return config.get("on_demo_server", False)
+    return False
