410a5173389ee29f1bf4a7aa7b725b1db5a1d6e9
Edan Bainglass <edan.bainglass@gmail.com>
2024-12-23 09:10:47 +0000

    Implement mechanism to override defaults using a custom config file

--- src/aiidalab_qe/app/parameters/__init__.py
+++ src/aiidalab_qe/app/parameters/__init__.py
@@ -1,7 +1,54 @@
+from collections.abc import Mapping
 from importlib import resources
+from pathlib import Path
 
 import yaml
 
 from aiidalab_qe.app import parameters
 
+
+def recursive_merge(d1, d2):
+    """Merge dictionary `d2` into dictionary `d1` recursively.
+
+    - For keys that are in both dictionaries:
+        - If values are dictionaries, merge them recursively.
+        - Otherwise, overwrite the value in `d1` with the value in `d2`.
+    - Keys in `d2` not in `d1` are added to `d1`.
+
+    Parameters
+    ----------
+    `d1` : `dict`
+        The dictionary to merge into.
+    `d2` : `dict`
+        The dictionary to merge from.
+
+    Returns
+    -------
+    `dict`
+        The merged dictionary.
+
+    Examples
+    --------
+    >>> d1 = {'a': 1, 'b': {'c': 2, 'd': 3}}
+    >>> d2 = {'b': {'c': 4, 'e': 5}}
+    >>> recursive_merge(d1, d2)
+    {'a': 1, 'b': {'c': 4, 'd': 3, 'e': 5}}
+    """
+    for key, value in d2.items():
+        if key in d1:
+            if isinstance(d1[key], Mapping) and isinstance(value, Mapping):
+                recursive_merge(d1[key], value)
+            else:
+                d1[key] = value
+        else:
+            d1[key] = value
+    return d1
+
+
 DEFAULT_PARAMETERS = yaml.safe_load(resources.read_text(parameters, "qeapp.yaml"))
+
+
+custom_config_file = Path.home() / ".aiidalab" / "quantumespresso" / "config.yml"
+if custom_config_file.exists():
+    custom_config = yaml.safe_load(custom_config_file.read_text())
+    DEFAULT_PARAMETERS = recursive_merge(DEFAULT_PARAMETERS, custom_config)
