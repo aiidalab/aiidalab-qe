526956960103f5f00ad0491830fd1b2db2ccecde
Edan Bainglass <edan.bainglass@gmail.com>
2024-12-24 11:59:15 +0000

    Fix test

--- tests/test_infobox.py
+++ tests/test_infobox.py
@@ -35,13 +35,8 @@ def test_in_app_guide():
     assert in_app_guide.layout.display == "none"
     guide_manager.active_guide = "general/basic"
     assert in_app_guide.layout.display == "flex"
-    guide_manager.active_guide = "general/advanced"
-    assert in_app_guide.layout.display == "flex"
-    in_app_guide.guide_id = "general/advanced"
-    guide_manager.active_guide = "general/basic"
+    guide_manager.active_guide = "none"
     assert in_app_guide.layout.display == "none"
-    guide_manager.active_guide = "general/advanced"
-    assert in_app_guide.layout.display == "flex"
 
     guide_manager.active_guide = "general/basic"
     in_app_guide = InAppGuide(identifier="guide-warning")
