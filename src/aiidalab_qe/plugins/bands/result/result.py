898d73dd991a3546bcb1f450992d3faed0838cf7
AndresOrtegaGuerrero <oandresg15@gmail.com>
2024-12-25 11:40:32 +0000

    removing layout in widget within result plugins

--- src/aiidalab_qe/plugins/bands/result/result.py
+++ src/aiidalab_qe/plugins/bands/result/result.py
@@ -1,7 +1,5 @@
 """Bands results view widgets"""
 
-import ipywidgets as ipw
-
 from aiidalab_qe.common.bands_pdos import BandsPdosModel, BandsPdosWidget
 from aiidalab_qe.common.panel import ResultsPanel
 
@@ -13,6 +11,5 @@ class BandsResultsPanel(ResultsPanel[BandsResultsModel]):
         bands_node = self._model.get_bands_node()
         model = BandsPdosModel()
         widget = BandsPdosWidget(model=model, bands=bands_node)
-        widget.layout = ipw.Layout(width="1000px")
         widget.render()
         self.children = [widget]
