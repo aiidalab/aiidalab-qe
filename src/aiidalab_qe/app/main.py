295d111f6479a3bb613d3f21b7a7881607091c3d
Edan Bainglass <edan.bainglass@gmail.com>
2025-02-03 13:44:10 +0000

    Add an optional logging output widget to the app

--- src/aiidalab_qe/app/main.py
+++ src/aiidalab_qe/app/main.py
@@ -5,6 +5,7 @@ Authors: AiiDAlab team
 
 from pathlib import Path
 
+import ipywidgets as ipw
 from IPython.display import display
 
 from aiidalab_qe.app.static import styles
@@ -20,12 +21,17 @@ DEFAULT_BUG_REPORT_URL = "https://github.com/aiidalab/aiidalab-qe/issues/new"
 
 class QeApp:
     def __init__(
-        self, process=None, qe_auto_setup=True, bug_report_url=DEFAULT_BUG_REPORT_URL
+        self,
+        process=None,
+        qe_auto_setup=True,
+        bug_report_url=DEFAULT_BUG_REPORT_URL,
+        show_log=False,
     ):
         """Initialize the AiiDAlab QE application with the necessary setup."""
 
         self.process = process
         self.qe_auto_setup = qe_auto_setup
+        self.log_widget = None
 
         self._load_styles()
 
@@ -33,6 +39,30 @@ class QeApp:
         self.model = AppWrapperModel()
         self.view = AppWrapperView()
         display(self.view)
+
+        if show_log:
+            self.log_widget = ipw.Output(
+                layout=ipw.Layout(
+                    border="solid 1px lightgray",
+                    margin="2px",
+                    padding="5px",
+                ),
+            )
+            reset_button = ipw.Button(
+                description="Clear log",
+                button_style="primary",
+                icon="trash",
+                layout=ipw.Layout(width="fit-content"),
+            )
+            reset_button.on_click(lambda _: self.log_widget.clear_output())
+            display(
+                ipw.VBox(
+                    children=[
+                        reset_button,
+                        self.log_widget,
+                    ],
+                )
+            )
 
         # Set up bug report handling (if a URL is provided)
         if bug_report_url:
@@ -52,7 +82,10 @@ class QeApp:
 
     def load(self):
         """Initialize the WizardApp and integrate the app into the main view."""
-        self.app = WizardApp(qe_auto_setup=self.qe_auto_setup)
+        self.app = WizardApp(
+            qe_auto_setup=self.qe_auto_setup,
+            log_widget=self.log_widget,
+        )
         self.view.main.children = [self.app]
         # load a previous calculation if it is provided
         if self.process:
