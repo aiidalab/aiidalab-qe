33db6742f9ef0aaa91915925228eb1e329b1b234
AndresOrtegaGuerrero <oandresg15@gmail.com>
2025-02-20 14:31:22 +0000

    setting the spin as 1 2

--- src/aiidalab_qe/app/configuration/advanced/hubbard/hubbard.py
+++ src/aiidalab_qe/app/configuration/advanced/hubbard/hubbard.py
@@ -185,7 +185,7 @@ class HubbardConfigurationSettingsPanel(
                         state_index=state_index,
                         kind_name=kind_name: update(
                             kind_index,
-                            1,
+                            0,
                             state_index,
                             kind_name,
                             float(value),
@@ -218,7 +218,7 @@ class HubbardConfigurationSettingsPanel(
                         state_index=state_index,
                         kind_name=kind_name: update(
                             kind_index,
-                            2,
+                            0,
                             state_index,
                             kind_name,
                             float(value),
