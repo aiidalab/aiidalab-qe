c5c105c0d3f67b1739aa3256d12ec23709b7cfcd
Edan Bainglass <edan.bainglass@gmail.com>
2025-02-24 13:41:30 +0000

    Define process state icons in common module

--- src/aiidalab_qe/common/process/__init__.py
+++ src/aiidalab_qe/common/process/__init__.py
@@ -1,6 +1,11 @@
 from .process import QeAppWorkChainSelector, WorkChainSelector
+from .state import STATE_ICONS
+from .tree import SimplifiedProcessTree, SimplifiedProcessTreeModel
 
 __all__ = [
+    "STATE_ICONS",
     "QeAppWorkChainSelector",
+    "SimplifiedProcessTree",
+    "SimplifiedProcessTreeModel",
     "WorkChainSelector",
 ]
