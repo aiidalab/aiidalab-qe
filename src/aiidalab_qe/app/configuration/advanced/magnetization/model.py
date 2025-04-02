b53d16e1324423e7963c1f3894275e2f7eee8b05
Timo Reents <timo.reents1@gmail.com>
2025-04-02 19:58:51 +0000

    Fix default magnetic moments for atoms with tags
    Currently, the default magnetic moments do not properly work when
    the atoms have tags assigned. This is for example the case when modelling
    antiferromagnetic systems.
    
    The problem was that we first populate `self._default_magnetic_moments`
    using the utility methods of the AiiDA-QE plugin. Those expect the chemical
    elements without any tags. Hence, when dealing with tags, `self._default_magnetic_moments`
    simply contained the elements with tags and 0.1 as magnetization. In the
    `_to_moment` we were correctly using the `kind.symbol`. However, we first
    tried to retrieve the values from `self._default_magnetic_moments`, which
    doesn't cotain them as outlined before, again resulting in the default
    magnetization of 0.1.
    
    In the fix, I simply combine both methods and direcrtly acccess
    the AiiDA-QE moments with the correct `kind.symbol` key.

--- src/aiidalab_qe/app/configuration/advanced/magnetization/model.py
+++ src/aiidalab_qe/app/configuration/advanced/magnetization/model.py
@@ -1,14 +1,11 @@
 from copy import deepcopy
 
 import traitlets as tl
-from aiida_pseudo.groups.family import PseudoPotentialFamily
 
 from aiida_quantumespresso.workflows.protocols.utils import (
     get_magnetization_parameters,
-    get_starting_magnetization,
 )
 from aiidalab_qe.common.mixins import HasInputStructure
-from aiidalab_qe.utils import fetch_pseudo_family_by_label
 
 from ..subsettings import AdvancedCalculationSubSettingsModel
 
@@ -56,14 +53,12 @@ class MagnetizationConfigurationSettingsModel(
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._default_starting_magnetization = {}
 
     def update(self, specific=""):  # noqa: ARG002
         if self.spin_type == "none" or not self.has_structure:
             self._defaults["moments"] = {}
         else:
             self.update_type_help()
-            self.update_default_starting_magnetization()
             self._update_default_moments()
         with self.hold_trait_notifications():
             self.moments = self._get_default_moments()
@@ -93,19 +88,6 @@ class MagnetizationConfigurationSettingsModel(
                 """
             )
 
-    def update_default_starting_magnetization(self):
-        """Update the default starting magnetization based on the structure and
-        pseudopotential family.
-        """
-        if not self.has_structure:
-            # TODO this guard shouldn't be here! It IS here only because in the present
-            # implementation, an update is called on app start. This breaks lazy loading
-            # and should be carefully checked!
-            return
-        family = fetch_pseudo_family_by_label(self.family)
-        initial_guess = get_starting_magnetization(self.input_structure, family)
-        self._default_starting_magnetization = initial_guess
-
     def reset(self):
         with self.hold_trait_notifications():
             self.type = self.traits()["type"].default_value
@@ -113,20 +95,10 @@ class MagnetizationConfigurationSettingsModel(
             self.moments = self._get_default_moments()
 
     def _update_default_moments(self):
-        family = fetch_pseudo_family_by_label(self.family)
         self._defaults["moments"] = {
-            kind.name: self._to_moment(symbol=kind.symbol, family=family)
+            kind.name: self._DEFAULT_MOMENTS.get(kind.symbol, {}).get("magmom", 0.1)
             for kind in self.input_structure.kinds
         }
-
-    def _to_moment(self, symbol: str, family: PseudoPotentialFamily) -> float:
-        """Convert the default magnetization to an initial magnetic moment."""
-        magnetization = (
-            self._default_starting_magnetization.get(symbol, 0.1)
-            if self._DEFAULT_MOMENTS.get(symbol, {}).get("magmom")
-            else 0.1
-        )
-        return round(magnetization * family.get_pseudo(symbol).z_valence, 3)
 
     def _get_default_moments(self):
         return deepcopy(self._defaults.get("moments", {}))
