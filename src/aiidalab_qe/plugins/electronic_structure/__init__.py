949d85f3a337eaf03779f7a84c92760de18fb81a
Edan Bainglass <edan.bainglass@gmail.com>
2025-05-24 05:45:16 +0000

    Discard electronic structure examples (merged with core)

--- src/aiidalab_qe/plugins/electronic_structure/__init__.py
+++ src/aiidalab_qe/plugins/electronic_structure/__init__.py
@@ -4,10 +4,5 @@ electronic_structure = {
     "result": {
         "panel": ElectronicStructureResultsPanel,
         "model": ElectronicStructureResultsModel,
-    },
-    "examples": {
-        "repo": "aiidalab/aiidalab-qe-examples",
-        "tag": "1.0",
-        "path": "electronic_structure",
-    },
+    }
 }
