a415edc132317e5097c8ea439b4c20d8195c4ba8
Edan Bainglass <edan.bainglass@gmail.com>
2026-09-09 13:30:39 +0200

    Bump version v26.06.11 -> v26.09.0rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ import time
 
 # -- Project information -----------------------------------------------------
 
-version = "v26.06.11"
+version = "v26.09.0rc1"
 release = f"{version}-dev"
 project = "Quantum ESPRESSO App"
 copyright_first_year = "2023"
